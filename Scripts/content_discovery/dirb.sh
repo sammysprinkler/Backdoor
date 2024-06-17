074dcfb609ab427c5d4303e4673871eca11989bb
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:00:24 -0400

    Create dirb.sh

---
+++ Scripts/content_discovery/dirb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Dirb - Web content scanner
+# Options:
+# <target_url> : The target URL
+# <wordlist> : Path to the wordlist
+
+# Usage: dirb <target_url> <wordlist>
+
+# Example script
+dirb http://example.com /path/to/wordlist.txt
