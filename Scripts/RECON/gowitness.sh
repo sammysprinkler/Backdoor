9515366512339e00b3fa2cf4d68762e6f0af5ba4
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:05:55 -0400

    Create gowitness.sh

---
+++ Scripts/RECON/gowitness.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Gowitness - Capture screenshots of web pages
+# Options:
+# file : Input file containing list of URLs
+
+# Usage: gowitness file -f <input_file>
+
+# Example script
+gowitness file -f httprobe_output.txt
