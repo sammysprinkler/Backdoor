730c1e2f49ecc4b68cc2040e6da92fac08b2e064
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:06:49 -0400

    Create aquatone.sh

---
+++ Scripts/RECON/aquatone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Aquatone - Tool for visual inspection of websites across a large number of hosts
+# Options:
+# -nmap : Path to Nmap XML output file
+
+# Usage: cat <input_file> | aquatone
+
+# Example script
+cat httprobe_output.txt | aquatone
