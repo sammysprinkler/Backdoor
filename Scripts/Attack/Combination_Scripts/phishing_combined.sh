d6d0da9f6744fba3af086bd780f8f0db71bfcb30
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:34:44 -0400

    Create phishing_combined.sh

---
+++ Scripts/Attack/Combination_Scripts/phishing_combined.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Phishing Combined Script
+# Uses SET for creating a phishing campaign and Evilginx2 for advanced phishing attacks.
+
+echo "Launching Social Engineering Toolkit (SET)..."
+setoolkit
+
+echo "Launching Evilginx2..."
+evilginx
