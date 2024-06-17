4e343568d91a94cfb3eb9254e7948cfb2c477dc3
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:01:10 -0400

    Create arjun.sh

---
+++ Scripts/parameter_analysis/arjun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Arjun - HTTP parameter discovery suite
+# Options:
+# -u : URL of the target web application
+# -o : Output file
+
+# Usage: arjun -u <target_url> -o <output_file>
+
+# Example script
+arjun -u http://example.com -o arjun_output.txt
