b29ec8fe0e7545550f7ac05833fc96abb961c317
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:24:54 -0400

    Update slowloris.sh

--- Scripts/Attack/Denial_of_Service/slowloris.sh
+++ Scripts/Attack/Denial_of_Service/slowloris.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Slowloris - DoS attack tool
+# Options:
+# -t : Number of threads
+# -p : Port
 
+# Usage: slowloris -t <threads> -p <port> <target_url>
+# Example script
+slowloris -t 100 -p 80 example.com
