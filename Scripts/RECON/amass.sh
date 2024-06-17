ebbf688b42a19e93ed0d962b9c1d268329146a00
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:04:10 -0400

    Create amass.sh

---
+++ Scripts/RECON/amass.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Amass - In-depth DNS enumeration and network mapping
+# Options:
+# enum : Perform enumeration
+# -d : Domain to enumerate
+# -o : Output file
+
+# Usage: amass enum -d <target_domain> -o <output_file>
+
+# Example script
+amass enum -d example.com -o amass_output.txt
