a31f8c1f3976770e2e3c2a51714de1b625c45f96
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:24:16 -0400

    Update ettercap_dns_spoof.sh

--- Scripts/Attack/DNS_Spoofing/ettercap_dns_spoof.sh
+++ Scripts/Attack/DNS_Spoofing/ettercap_dns_spoof.sh
@@ -1,1 +1,10 @@
-.
+#!/bin/bash
+# Ettercap - DNS Spoofing tool
+# Options:
+# -T : Text mode
+# -q : Quiet mode
+# -i : Interface
+
+# Usage: ettercap -T -q -i <interface> -P dns_spoof
+# Example script
+ettercap -T -q -i eth0 -P dns_spoof
