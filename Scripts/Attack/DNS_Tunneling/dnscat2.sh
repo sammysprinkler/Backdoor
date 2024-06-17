07ec68e2941316aa47be338c6fcaf0d4b74d9616
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:24:41 -0400

    Update dnscat2.sh

--- Scripts/Attack/DNS_Tunneling/dnscat2.sh
+++ Scripts/Attack/DNS_Tunneling/dnscat2.sh
@@ -1,1 +1,9 @@
-.
+#!/bin/bash
+# DNSCat2 - DNS Tunneling tool
+# Options:
+# -d : Domain
+# -c : Command
+
+# Usage: dnscat2 -d <target_domain> -c <command>
+# Example script
+dnscat2 -d example.com -c "ls -la"
