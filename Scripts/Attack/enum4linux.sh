4993356727c7338cdd498e09dc028073c00f0a48
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:14:09 -0400

    Create enum4linux.sh

---
+++ Scripts/Attack/enum4linux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Enum4Linux - Enumeration tool for Windows/Linux systems
+# Options:
+# -a : All basic enumeration options
+
+# Usage: enum4linux -a <target_ip>
+# Example script
+enum4linux -a 192.168.1.1
