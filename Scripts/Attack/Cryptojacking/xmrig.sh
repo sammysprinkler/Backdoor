2e6e5bc7e66e2cf713c23f7bf608273f0210cbac
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:23:51 -0400

    Update xmrig.sh

--- Scripts/Attack/Cryptojacking/xmrig.sh
+++ Scripts/Attack/Cryptojacking/xmrig.sh
@@ -1,1 +1,10 @@
-.
+#!/bin/bash
+# XMRig - Monero mining tool
+# Options:
+# -o : Pool URL
+# -u : Username
+# -p : Password
+
+# Usage: xmrig -o <pool_url> -u <username> -p <password>
+# Example script
+xmrig -o pool.supportxmr.com:3333 -u 42XbJSoFqXoZLXM6LjQmNSkpMFszyKmowabnPrfF9R4VmpWRCv8 -p x
