8760818860781d1d14299678627e7d1f91350b26
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:04:17 -0400

    Create assetfinder.sh

---
+++ Scripts/RECON/assetfinder.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Assetfinder - Find related domains and subdomains
+# Options:
+# --subs-only : Only find subdomains
+
+# Usage: assetfinder --subs-only <target_domain>
+
+# Example script
+assetfinder --subs-only example.com > assetfinder_output.txt
