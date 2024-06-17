60bd5e023e8a5c646023c57a9620b29eddd95118
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:04:39 -0400

    Create waybackurls.sh

---
+++ Scripts/RECON/waybackurls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Waybackurls - Fetch URLs from the Wayback Machine
+# Options:
+# None
+
+# Usage: echo <target_domain> | waybackurls
+
+# Example script
+echo example.com | waybackurls > waybackurls_output.txt
