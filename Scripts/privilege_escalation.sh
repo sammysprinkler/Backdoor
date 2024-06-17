5d8b1783350abc8319d513dfb9faaedc62dec570
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 12:47:11 -0400

    Create privilege_escalation.sh

---
+++ Scripts/privilege_escalation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Privilege escalation - Check for sudo privileges
+# Options:
+# -l : List the allowed (and forbidden) commands for the invoking user
+
+# Usage: sudo -l
+
+# Example script
+sudo -l
