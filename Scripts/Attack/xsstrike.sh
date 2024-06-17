0a4f2c8f0610053dde45f30c56f59af0bbfeb70f
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:13:01 -0400

    Create xsstrike.sh

---
+++ Scripts/Attack/xsstrike.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# XSStrike - Advanced XSS detection suite
+# Options:
+# -u : URL to scan
+
+# Usage: xsstrike -u <target_url>
+# Example script
+xsstrike -u http://example.com/vuln.php
