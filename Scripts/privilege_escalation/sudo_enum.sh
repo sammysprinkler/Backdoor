c340531bc80464ce531e4f091e36937e8520c713
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 14:11:46 -0400

    Create sudo_enum.sh

---
+++ Scripts/privilege_escalation/sudo_enum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Sudo Enum - Enumerate sudo permissions
+# Options:
+# None
+
+# Usage: sudo_enum.sh
+# Example script
+# sudo_enum.sh
+
+echo "Starting Sudo Enumeration"
+
+# Perform Sudo Enumeration
+sudo -l
+
+echo "Sudo Enumeration completed."
