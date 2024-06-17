e23f5623f5cf88ab8461c35f283b0cea4cd4b702
sammysprinkler <168591441+sammysprinkler@users.noreply.github.com>
2024-06-17 13:01:27 -0400

    Create heatmap.sh

---
+++ Scripts/heatmapping/heatmap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Heatmap - Visualizing web application usage
+# Options:
+# -u : URL of the target web application
+# -o : Output file
+
+# Usage: heatmap -u <target_url> -o <output_file>
+
+# Example script
+heatmap -u http://example.com -o heatmap_output.html
